3adecf3d1f20d8ca4224d208d226bb04f748dbef
LILIBO <78360159@qq.com>
2021-08-07 17:20:34 +0800

    dev commit

---
+++ src/test/java/com/bobo/Test.java
@@ -0,0 +1,15 @@
+package com.bobo;
+
+/**
+ * Test
+ *
+ * @author LILIBO
+ * @since 2021-08-07
+ */
+public class Test {
+
+    @org.junit.Test
+    public void tester() {
+        System.out.println("Test by dev!");
+    }
+}
